ccc26fa75607fe4b1452d32592c66b3000c55e83
Builder User <keith.dart@gmail.com>
2013-03-05 11:14:34 -0800

    Some simple handler updates. This is still just testing overall design and proof of concept. Not usable yet.

--- source/router/ui/diagnostics.py
+++ source/router/ui/diagnostics.py
@@ -4,7 +4,10 @@ from __future__ import print_function
 from __future__ import unicode_literals
 from __future__ import division
 
+import os
+
 from pycopia.WWW import framework
+#from pycopia import proctools
 
 from router.ui import auth
 
@@ -17,11 +20,44 @@ class DiagnosticsHandler(framework.RequestHandler):
         doc = resp.doc
         doc.stylesheet = "/media/css/ui.css"
         doc.scripts = ["/media/js/modernizr-1.7.min.js"]
-        doc.title = "Router Diagnostics"
+        doc.title = "Router Diagnostics (this is a test)"
         nav = doc.body.add_section("nav")
         nav.append(resp.anchor2("/", "Home"))
-        doc.body.add_header(1, "Router Diagnostics")
+        doc.body.add_header(1, "Router Diagnostics (TEST)")
+        doc.body.add_unordered_list(get_files())
+        frm = doc.add_form()
+        frm.add_textinput("rcpt", "Text")
+        frm.add_input(type="submit", value="Submit")
         return resp.finalize()
+
+    def post(self, request):
+        resp = self.get_response(request)
+        doc = resp.doc
+        text = request.POST["rcpt"]
+        doc.new_para("You entered:")
+        doc.new_preformat(text)
+        doc.new_para("Report desc:")
+        doc.new_preformat(get_report())
+        return resp.finalize()
+
+def get_files():
+    rv = []
+    for name in os.listdir("/proc/self/fd"):
+        try:
+            rv.append("{} - > {}".format(name, os.readlink("/proc/self/fd/" + name)))
+        except OSError:
+            rv.append("{} did not read".format(name))
+    return rv
+
+def get_report():
+    return """
+        User id: {uid}
+    Current dir: {cwd}
+    """.format(
+            uid=os.getuid(),
+            cwd=os.getcwd(),
+            )
+
 
 index = DiagnosticsHandler(verifier=auth.need_login)
 
--- source/router/ui/status.py
+++ source/router/ui/status.py
@@ -8,24 +8,39 @@ from __future__ import division
 
 from pycopia import passwd
 from pycopia.WWW import framework
+from pycopia.WWW import HTML5
 
 from router.ui import auth
 from router import controller
 
 ENCODING = b"utf-8"
 
+
+def status_constructor(**kwargs):
+    doc = HTML5.new_document()
+    for name, val in kwargs.items():
+        setattr(doc, name, val)
+    doc.stylesheet = "/media/css/ui.css"
+    doc.scripts = ["/media/js/modernizr-1.7.min.js"]
+    navigation = doc.body.add_section("nav", id="navigation")
+    header = doc.add_section("header", id="header")
+    content = wrapper.add_section("container", id="content")
+    footer = doc.add_section("footer", id="footer")
+    doc.header = header
+    doc.content = content
+    doc.nav = navigation
+    doc.footer = footer
+    return doc
+
+
+
 class StatusHandler(framework.RequestHandler):
 
     def get(self, request):
-        resp = self.get_response(request)
+        resp = self.get_response(request, title="Router Status")
         doc = resp.doc
-        doc.stylesheet = "/media/css/ui.css"
-        doc.scripts = ["/media/js/modernizr-1.7.min.js"]
-        doc.title = "Router Status"
-        nav = doc.body.add_section("nav")
-        nav.append(resp.anchor2("/", "Home"))
+        doc.nav.append(resp.anchor2("/", "Home"))
         doc.body.add_header(1, "Interface Status")
-
         cont = controller.RouterManager()
         iflist = cont.get_interfaces()
         doc.new_preformat(str(iflist)) # XXX
@@ -37,11 +52,18 @@ class DashboardHandler(framework.RequestHandler):
     def get(self, request):
         resp = self.get_response(request)
         doc = resp.doc
-        doc.stylesheet = "/media/css/ui.css"
-        doc.new_para("Current Status")
+        doc.new_para("Interface Status")
+        ifstat = ["{}: {}".format(ifs[b"name"], ifs[b"status"]) for ifs in get_interface_status()]
+        doc.body.add_unordered_list(ifstat)
         return resp.finalize()
 
 
-index = StatusHandler(verifier=auth.need_login)
+
+def get_interface_status():
+        cont = controller.RouterManager()
+        return cont.get_interfaces().values()
+
+
+index = StatusHandler(constructor=status_constructor, verifier=auth.need_login)
 dashboard = DashboardHandler(verifier=auth.need_login)
 
